ccf007c0e230659819036c436e47ff3233727f84
Mehul Srivastava <mehulsrivastava1206@gmail.com>
2024-10-04 22:03:09 +0530

    video-processor: polls sqs and spins up container on ecs

--- apps/video-processor/src/index.ts
+++ apps/video-processor/src/index.ts
@@ -1,2 +1,30 @@
-const b = 12 + 13;
-console.log(b);
+import dotenv from "dotenv";
+import { ReceiveMessageCommand, SQSClient } from "@aws-sdk/client-sqs";
+
+dotenv.config();
+
+const sqsClient = new SQSClient({
+  region: "eu-north-1",
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+  },
+});
+
+const params = {
+  QueueUrl: process.env.AWS_SQS_QUEUE_URL!,
+  MaxNumberOfMessages: 10,
+  VisibilityTimeout: 30,
+  WaitTimeSeconds: 20,
+};
+
+async function init() {
+  while (true) {
+    const command = new ReceiveMessageCommand(params);
+    const response = await sqsClient.send(command);
+    console.log(response);
+    // spin container on ecs fargate
+  }
+}
+
+init();
