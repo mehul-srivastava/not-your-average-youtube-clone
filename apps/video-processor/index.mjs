6deb9c83210be4011df7c18bc852b4433d8fd507
Mehul Srivastava <mehulsrivastava1206@gmail.com>
2024-10-07 21:10:27 +0530

    video-processor: add sqs message receipt handle to env vars

---
+++ apps/video-processor/index.mjs
@@ -0,0 +1,64 @@
+import { ECSClient, RunTaskCommand } from "@aws-sdk/client-ecs";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const client = new ECSClient({
+  region: "eu-north-1",
+});
+
+const params = {
+  cluster: process.env.AWS_ECS_CLUSTER_ARN,
+  taskDefinition: process.env.AWS_ECS_TASK_DEFINITION_ARN,
+  launchType: "FARGATE",
+  networkConfiguration: {
+    awsvpcConfiguration: {
+      assignPublicIp: "ENABLED",
+      securityGroups: ["sg-0115108e2baf6fab0"],
+      subnets: [
+        "subnet-073a9e8fbb5ccde3d",
+        "subnet-08b5a974ad9b57669",
+        "subnet-0259e841c4ac7df44",
+        "subnet-0ea4eeb2af28461ad",
+      ],
+    },
+  },
+  overrides: {
+    containerOverrides: [
+      {
+        name: process.env.AWS_ECS_CONTAINER_NAME,
+        environment: [
+          {
+            name: "KEY",
+            value: "", // inserted by handler
+          },
+          {
+            name: "RECEIPT_HANDLE",
+            value: "",
+          },
+          {
+            name: "BUCKET_NAME",
+            value: process.env.AWS_S3_BUCKET_NAME,
+          },
+        ],
+      },
+    ],
+  },
+};
+
+export const handler = async (event) => {
+  const videoKey = event.Records[0].body.Records[0].s3.object.key;
+  const receiptHandle = event.Records[0].receiptHandle;
+
+  params.overrides.containerOverrides[0].environment[0].value = videoKey;
+  params.overrides.containerOverrides[0].environment[1].value = receiptHandle;
+
+  const command = new RunTaskCommand(params);
+  const response = await client.send(command);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: "Task command sent" }),
+    response,
+  };
+};
