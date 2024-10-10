bd7b3dc7f9673e4a9a4873419e2663312e460f18
Mehul Srivastava <mehulsrivastava1206@gmail.com>
2024-10-10 13:44:07 +0530

    platform: store raw videos in folders

--- apps/platform/app/api/get-presigned-url/route.ts
+++ apps/platform/app/api/get-presigned-url/route.ts
@@ -1,20 +1,30 @@
 import { NextRequest, NextResponse } from "next/server";
 import { PutObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import crypto from "node:crypto";
 
 import s3Client from "@/clients/s3-client";
 
 export async function POST(request: NextRequest) {
   const { filename }: { filename: string } = await request.json();
 
-  const key = filename.substring(0, filename.length - 4) + new Date().getTime() + ".mp4";
+  const timestamp = new Date().getTime().toString();
+
+  const folderName = crypto
+    .createHash("sha256")
+    .update(timestamp)
+    .digest("hex")
+    .slice(0, 25)
+    .concat("/");
+  const objectKey =
+    folderName + filename.substring(0, filename.length - 4) + "_" + timestamp + ".mp4";
 
   const command = new PutObjectCommand({
     Bucket: "youtube-clone-temporary",
-    Key: key,
+    Key: objectKey,
   });
 
-  const url = await getSignedUrl(s3Client, command, { expiresIn: 30 });
+  const url = await getSignedUrl(s3Client, command, { expiresIn: 60 });
 
-  return NextResponse.json({ url });
+  return NextResponse.json({ url: url });
 }
