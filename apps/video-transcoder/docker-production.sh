5091c1570dd5e26cb032661c1f63eae94cc20b1f
Mehul Srivastava <mehulsrivastava1206@gmail.com>
2024-10-26 12:23:30 +0530

    video-transcoder: rename service

--- apps/video-transcoder/docker-production.sh
+++ apps/video-transcoder/docker-production.sh
@@ -10,3 +10,5 @@ npm run build
 docker build -t youtube-clone/video-transcoder .
 docker tag youtube-clone/video-transcoder:latest 886436961672.dkr.ecr.eu-north-1.amazonaws.com/youtube-clone/video-transcoder:latest
 docker push 886436961672.dkr.ecr.eu-north-1.amazonaws.com/youtube-clone/video-transcoder:latest
+
+echo "Docker image pushed to AWS registry"
